689f8f54e3bed7d24b0754357da92dbc4ea92ad4
collins njau <collinsnjau39@gmail.com>
2020-09-03 14:41:10 +0300

    Update the user shema to include the google provider email

--- api/models/user.js
+++ api/models/user.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   email: {
-    type: String,
-    required: true,
-    unique: true
+    type: String, required: true,
+    trim: true, unique: true,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
   },
-  password: { type: String, required: true }
+  googleProvider: {
+    type: {
+        id: String,
+        token: String
+    },
+    select: false
+}
 });
 
 module.exports = mongoose.model("User", userSchema);

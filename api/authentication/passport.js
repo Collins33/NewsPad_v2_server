9d88face11952783833949e874f72e13531de672
collins njau <collinsnjau39@gmail.com>
2020-09-18 13:15:19 +0300

    Remove passport related code

--- api/authentication/passport.js
+++
@@ -1,5 +0,0 @@
-const passport = require('passport');
-// instruct Passport to use new instance of Google strategy
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-passport.use(new GoogleStrategy());
